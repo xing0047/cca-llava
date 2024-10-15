d7e6fae67d1d249facd892f0805423d78e8dccf8
xing0047 <xing0047@e.ntu.edu.sg>
2024-10-15 16:39:05 +0800

    eval.

--- llava/eval/model_vqa_pope.py
+++ llava/eval/model_vqa_pope.py
@@ -36,8 +36,10 @@ def eval_model(args):
     model_path = os.path.expanduser(args.model_path)
     model_name = get_model_name_from_path(model_path)
     tokenizer, model, image_processor, context_len = load_pretrained_model(model_path, args.model_base, model_name)
-
-    questions = [json.loads(q) for q in open(os.path.expanduser(args.question_file), "r")]
+    if 'coco' in args.question_file:
+        questions = [json.loads(q) for q in open(os.path.expanduser(args.question_file), "r")]
+    else:
+        questions = json.load(open(args.question_file))
     questions = get_chunk(questions, args.num_chunks, args.chunk_idx)
     answers_file = os.path.expanduser(args.answers_file)
     os.makedirs(os.path.dirname(answers_file), exist_ok=True)
